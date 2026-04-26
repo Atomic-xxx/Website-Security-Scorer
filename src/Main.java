ad213c5899a65f36656a7b3404d951529a864bdd
Atomic-xxx <mayank.13646@stu.upes.ac.in>
2026-04-26 22:27:04 +0530

    Mayank

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,7 @@
-import service.SecurityAnalyzer;
-import model.ScanResult;
-import database.ResultDAO;
+import ui.MainFrame;
 
 public class Main {
     public static void main(String[] args) {
-
-        String url = "https://google.com";
-
-        SecurityAnalyzer analyzer = new SecurityAnalyzer();
-
-        ScanResult result = analyzer.analyze(url);
-
-        System.out.println("Total Score: " + result.getTotalScore());
-        
-         ResultDAO dao = new ResultDAO();
-         dao.saveResult(result);
+        new MainFrame().setVisible(true);
     }
 }
---
+++ src/ui/MainFrame.java
@@ -0,0 +1,80 @@
+package ui;
+
+import javax.swing.*;
+import java.awt.*;
+
+import service.SecurityAnalyzer;
+import model.ScanResult;
+import database.ResultDAO;
+
+public class MainFrame extends JFrame {
+
+    private JTextField urlField;
+    private JTextArea resultArea;
+    private JLabel scoreLabel;
+
+    public MainFrame() {
+
+        setTitle("Website Security Scorer");
+        setSize(500, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        // Top
+        JPanel top = new JPanel();
+        urlField = new JTextField(25);
+        JButton scanBtn = new JButton("Scan");
+        JButton saveBtn = new JButton("Save");
+
+        top.add(new JLabel("Enter URL:"));
+        top.add(urlField);
+        top.add(scanBtn);
+        top.add(saveBtn);
+
+        // Center
+        resultArea = new JTextArea();
+        resultArea.setEditable(false);
+
+        // Bottom
+        scoreLabel = new JLabel("Total Score: 0");
+
+        add(top, BorderLayout.NORTH);
+        add(new JScrollPane(resultArea), BorderLayout.CENTER);
+        add(scoreLabel, BorderLayout.SOUTH);
+
+        // Actions
+        scanBtn.addActionListener(e -> scan());
+        saveBtn.addActionListener(e -> save());
+    }
+
+    private void scan() {
+        String url = urlField.getText().trim();
+
+        SecurityAnalyzer analyzer = new SecurityAnalyzer();
+        ScanResult result = analyzer.analyze(url);
+
+        StringBuilder output = new StringBuilder();
+
+        for (String key : result.getBreakdown().keySet()) {
+            output.append(key)
+                  .append(": ")
+                  .append(result.getBreakdown().get(key))
+                  .append("\n");
+        }
+
+        resultArea.setText(output.toString());
+        scoreLabel.setText("Total Score: " + result.getTotalScore());
+    }
+
+    private void save() {
+        String url = urlField.getText().trim();
+
+        SecurityAnalyzer analyzer = new SecurityAnalyzer();
+        ScanResult result = analyzer.analyze(url);
+
+        ResultDAO dao = new ResultDAO();
+        dao.saveResult(result);
+
+        JOptionPane.showMessageDialog(this, "Saved to database!");
+    }
+}
